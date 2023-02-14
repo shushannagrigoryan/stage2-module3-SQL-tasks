9a12e7a7bdff73f04d841da5461873c6aa9d7e9a
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-02-14 23:02:26 +0400

    9

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -33,28 +33,48 @@ INSERT INTO paymenttype (`id`, `name`) VALUES(3, 'MONTHLY');
 
 
 
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (1, 2, 50.20, 1, '2020-03-03');
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (2, 3, 80.50, 4, '2021-03-03');
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (3, 2, 70.30, 7, '2022-03-03');
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (4, 1, 20.20, 5, '2022-05-03');
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (1, 2, 50.20, 1, '2020-03-03');
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (2, 3, 80.50, 4, '2021-03-03');
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (3, 2, 70.30, 7, '2022-03-03');
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (4, 1, 20.20, 5, '2022-05-03');
 
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (5, 1, 50.20, 2, '2020-03-03');
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (6, 2, 80.50, 6, '2021-03-03');
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (7, 3, 70.30, 3, '2022-03-03');
-INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (8, 4, 20.20, 7, '2022-05-03');
-
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (1, 2, 1, 8);
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (2, 4, 4, 5);
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (3, 5, 3, 9);
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (4, 8, 1, 6);
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (5, 9, 5, 9);
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (5, 1, 50.20, 2, '2020-03-03');
+--INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (6, 2, 80.50, 6, '2021-03-03');
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (7, 3, 70.30, 3, '2022-03-03');
+-- INSERT INTO payment (`id`, type_id, amount, student_id, payment_date) VALUES (8, 4, 20.20, 7, '2022-05-03');
 
 
 
 
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (6, 10, 7, 5);
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (7, 11, 8, 6);
-INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (8, 12, 9, 8);
+insert into payment (type_id, amount, payment_date, student_id) values (2, 50.20, '2017-05-13 10:48:47', 1);
+insert into payment (type_id, amount, payment_date, student_id) values (3, 80.50, '2017-05-13 10:48:47', 4);
+insert into payment (type_id, amount, payment_date, student_id) values (2, 70.30, '2017-05-13 10:48:47', 7);
+insert into payment (type_id, amount, payment_date, student_id) values (1, 20.20, '2017-05-13 10:48:47', 5);
+insert into payment (type_id, amount, payment_date, student_id) values (1, 50.20, '2017-05-13 10:48:47', 2);
+insert into payment (type_id, amount, payment_date, student_id) values (2, 50.20, '2017-05-13 10:48:47', 6);
+insert into payment (type_id, amount, payment_date, student_id) values (3, 50.20, '2017-05-13 10:48:47', 3);
+insert into payment (type_id, amount, payment_date, student_id) values (1, 50.20, '2017-05-13 10:48:47', 7);
+
+
+
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (1, 2, 1, 8);
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (2, 4, 4, 5);
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (3, 5, 3, 9);
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (4, 8, 1, 6);
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (5, 9, 5, 9);
+
+
+insert into mark (student_id, subject_id, mark) values (2, 1, 8);
+insert into mark (student_id, subject_id, mark) values (4, 4, 5);
+insert into mark (student_id, subject_id, mark) values (5, 3, 9);
+insert into mark (student_id, subject_id, mark) values (8, 1, 6);
+insert into mark (student_id, subject_id, mark) values (9, 5, 9);
+
+
+
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (6, 10, 7, 5);
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (7, 11, 8, 6);
+-- INSERT INTO mark(`id`, student_id , subject_id, mark ) VALUES (8, 12, 9, 8);
 
 
 
