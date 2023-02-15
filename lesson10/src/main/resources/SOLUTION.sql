ccfc48e810b5bbf9d973a6e0203ad2a4af357b24
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-02-15 14:45:43 +0400

    18

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,6 +1,6 @@
-SELECT * FROM subject JOIN mark ON subject.id = mark.subject_id GROUP BY subject.id HAVING AVG(mark.mark) > (SELECT AVG(mark) FROM mark);
+SELECT subject.* FROM subject  JOIN mark ON subject.id = mark.subject_id GROUP BY subject.id HAVING AVG(mark.mark) > (SELECT AVG(mark) FROM mark);
 
-SELECT * FROM student JOIN  payment ON student.id = payment.student_id GROUP BY student.id HAVING SUM(payment.amount) < (SELECT AVG(amount) FROM payment);
+SELECT student.* FROM student JOIN  payment ON student.id = payment.student_id GROUP BY student.id HAVING SUM(payment.amount) < (SELECT AVG(amount) FROM payment);
 
 
 
